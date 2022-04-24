791d954d2dccec578551001e540e8ce547c03a65
user <user@courseGIT.pl>
2022-04-24 11:40:36 +0200

    added Category list

--- Library/settings.py
+++ Library/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'library.urls'
+ROOT_URLCONF = 'Library.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'library.wsgi.application'
+WSGI_APPLICATION = 'Library.wsgi.application'
 
 
 # Database
