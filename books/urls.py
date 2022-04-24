9a838a262994e29c312af11516d854336a32f393
user <user@courseGIT.pl>
2022-04-24 11:40:12 +0200

    added Category list

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from books.views import get_hello_world, get_uuids_list_a, get_uuids_list_b, get_argument_from_path, \
-    get_arguments_from_query, check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView
+    get_arguments_from_query, check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, \
+    CategoryListTemplateView
 
 urlpatterns = [
     path('', get_hello_world, name="home"),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('get-headers/', get_headers, name="get_headers"),
     path('raise-error/', raise_error_for_fun, name="raise_error"),
     path('author-list', AuthorListBaseView.as_view(), name="author-list"),
+    path('category-list', CategoryListTemplateView.as_view(), name="category-list"),
 ]
